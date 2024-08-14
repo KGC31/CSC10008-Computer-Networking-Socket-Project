ecfbdb046e8bf5ecc3107a641a035f4eef43e525
KGC31 <kimnguyenblc@gmail.com>
2024-08-14 18:34:32 +0700

    Update folder structure

--- src/client/main.py
+++ src/client/main.py
@@ -2,7 +2,6 @@ import socket
 import json
 import os
 import hashlib
-import time
 from tqdm import tqdm
 
 # Client configuration
@@ -14,87 +13,127 @@ OUTPUT_DIR = 'downloads'
 
 downloaded = []
 
-# Read file list from input.txt
 def read_input_file():
+    """Read the list of files to download from the input file."""
     with open(INPUT_FILE, 'r') as f:
-        files_to_download = [line.strip() for line in f if line.strip()]
-    return files_to_download
+        return [line.strip() for line in f if line.strip()]
 
 def generate_checksum(data):
+    """Generate an MD5 checksum for the given data."""
     return hashlib.md5(data).hexdigest()
 
-def start_client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
-    print("Connected to the server.")
-
-    # Receive file list from server
+def receive_file_list(client_socket):
+    """Receive and display the list of available files from the server."""
     file_list_data = client_socket.recv(1024)
     file_list = json.loads(file_list_data.decode())
     print("Available files from server:")
     for file_name, info in file_list.items():
-        print(f"{file_name} - {info['size']} bytes")
+        print(f"{file_name} - {info['size']}")
+    return file_list
 
+def setup_output_directory():
+    """Create the output directory if it does not exist."""
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+def check_existing_files(files_to_download):
+    """Check if files already exist in the output directory and prompt user."""
+    filtered_files = []
+    for file_name in files_to_download:
+        file_path = os.path.join(OUTPUT_DIR, file_name)
+        if os.path.exists(file_path):
+            user_input = input(f"File '{file_name}' already exists. Overwrite? (y/n): ").strip().lower()
+            if user_input == 'y':
+                filtered_files.append(file_name)
+        else:
+            filtered_files.append(file_name)
+    return filtered_files
+
+def send_download_request(client_socket, files_to_download):
+    """Send the download request to the server."""
+    client_socket.sendall(json.dumps({'action': 'download', 'files': files_to_download}).encode())
+
+def process_server_response(client_socket, files_to_download):
+    """Process the server's response to download the files."""
+    buffer = b''
+    file = None
+    pbar = None
+
+    while files_to_download:
+        data = client_socket.recv(CHUNK_SIZE)
+        buffer += data
+
+        while True:
+            try:
+                response, offset = json.JSONDecoder().raw_decode(buffer.decode('latin1'))
+                buffer = buffer[offset:]
+
+                if response['type'] == 'start':
+                    file_name, file, pbar = start_file_transfer(response)
+
+                elif response['type'] == 'chunk':
+                    process_file_chunk(response, file, pbar)
+
+                elif response['type'] == 'end':
+                    complete_file_transfer(response, file, pbar, files_to_download)
+
+                elif response.get('action') == 'shutdown':
+                    print("Server is shutting down. Closing client.")
+                    return
+
+            except json.JSONDecodeError:
+                break
+
+def start_file_transfer(response):
+    """Handle the start of a file transfer."""
+    file_name = response['filename']
+    file_path = os.path.join(OUTPUT_DIR, file_name)
+    file_size = response['size']
+    file = open(file_path, 'wb')
+    pbar = tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name)
+    return file_name, file, pbar
+
+def process_file_chunk(response, file, pbar):
+    """Process a chunk of data received from the server."""
+    chunk = response['chunk'].encode('latin1')
+    checksum = response['checksum']
+    if generate_checksum(chunk) == checksum:
+        file.write(chunk)
+        pbar.update(len(chunk))
+
+def complete_file_transfer(response, file, pbar, files_to_download):
+    """Handle the completion of a file transfer."""
+    file_name = response['filename']
+    file.close()
+    pbar.close()
+    print(f"Download complete: {file_name}")
+    downloaded.append(file_name)
+    files_to_download.remove(file_name)
+
+def start_client():
+    """Start the client to connect to the server and download files."""
+    global downloaded
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    print("Connected to the server.")
+
+    file_list = receive_file_list(client_socket)
+    setup_output_directory()
+
     try:
         while True:
-            files_to_download = read_input_file()
+            queue = read_input_file()
+            queue = [file for file in queue if file not in downloaded]
 
-            # Filter out already downloaded files
-            files_to_download = [file for file in files_to_download if file not in downloaded]
-
-            if files_to_download:
-                # Send the list of files to download to the server
-                client_socket.sendall(json.dumps({'action': 'download', 'files': files_to_download}).encode())
-
-                buffer = b''
-                file = None
-                pbar = None
-
-                while files_to_download:
-                    data = client_socket.recv(CHUNK_SIZE)
-                    buffer += data
-
-                    # Process complete JSON objects from the buffer
-                    while True:
-                        try:
-                            response, offset = json.JSONDecoder().raw_decode(buffer.decode('latin1'))
-                            buffer = buffer[offset:]
-
-                            if response['type'] == 'start':
-                                file_name = response['filename']
-                                file_path = os.path.join(OUTPUT_DIR, file_name)
-                                file_size = response['size']
-                                file = open(file_path, 'wb')
-                                pbar = tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name)
-
-                            elif response['type'] == 'chunk':
-                                chunk = response['chunk'].encode('latin1')
-                                checksum = response['checksum']
-                                if generate_checksum(chunk) == checksum:
-                                    file.write(chunk)
-                                    pbar.update(len(chunk))
-
-                            elif response['type'] == 'end':
-                                file_name = response['filename']
-                                file.close()
-                                pbar.close()
-                                print(f"Download complete: {file_name}")
-                                downloaded.append(file_name)
-                                files_to_download.remove(file_name)
-
-                        except json.JSONDecodeError:
-                            # Not a complete JSON object, wait for more data
-                            break
-
-            time.sleep(2)  # Check the input file periodically
+            if queue:
+                queue = check_existing_files(queue)
+                if queue:  # Only proceed if there are files left to download
+                    send_download_request(client_socket, queue)
+                    process_server_response(client_socket, queue)
 
     except KeyboardInterrupt:
-        print("\nClient is shutting down...")
-        client_socket.sendall(json.dumps({'action': 'shutdown'}).encode())
-    finally:
+        print("Client interrupted. Closing connection.")
         client_socket.close()
 
 if __name__ == "__main__":
--- src/server/main.py
+++ src/server/main.py
@@ -8,32 +8,84 @@ import signal
 HOST = "192.168.1.172"
 PORT = 8000
 CHUNK_SIZE = 1024
-FILES_DIR = 'files'
+FILES_DIR = 'test_files'
+FILES_LIST = 'files_list.txt'
 
-# Flag for shutting down the server
+# Global flag for shutting down the server
 shutdown_flag = False
+client_conn = None  # Keep track of the client connection to send shutdown signal
 
-# Load the file list from the directory
-def load_file_list():
+def load_file_list(files_list_path):
+    """Load the list of available files from a file."""
     file_list = {}
-    for filename in os.listdir(FILES_DIR):
-        file_path = os.path.join(FILES_DIR, filename)
-        if os.path.isfile(file_path):  # Ensure it's a file and not a directory
-            file_size = os.path.getsize(file_path)  # Get the file size in bytes
-            file_list[filename] = {'size': file_size}
+    try:
+        with open(files_list_path, 'r') as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) >= 2:
+                    filename = parts[0]
+                    size_str = ' '.join(parts[1:])
+                    file_list[filename] = {'size': size_str}
+    except FileNotFoundError:
+        print(f"File {files_list_path} not found.")
     return file_list
 
-# Generate checksum for data
 def generate_checksum(data):
+    """Generate an MD5 checksum for a chunk of data."""
     return hashlib.md5(data, usedforsecurity=False).hexdigest()
 
-# Handle client connection
-def handle_client(conn, addr, file_list):
-    print(f'Connected by {addr}')
-    
-    # Send the file list to the client
+def send_file_list(conn, file_list):
+    """Send the list of available files to the client."""
     conn.sendall(json.dumps(file_list).encode())
 
+def send_file_start(conn, file_name, file_size):
+    """Send the start of file marker to the client."""
+    conn.sendall(json.dumps({'type': 'start', 'filename': file_name, 'size': file_size}).encode())
+
+def send_file_chunk(conn, file_name, chunk):
+    """Send a file chunk to the client."""
+    checksum = generate_checksum(chunk)
+    conn.sendall(json.dumps({
+        'type': 'chunk',
+        'filename': file_name,
+        'chunk': chunk.decode('latin1'),
+        'checksum': checksum
+    }).encode())
+
+def send_file_end(conn, file_name):
+    """Send the end of file marker to the client."""
+    conn.sendall(json.dumps({'type': 'end', 'filename': file_name}).encode())
+
+def handle_download_request(conn, file_list, files_to_download):
+    """Handle a download request from the client."""
+    for file_name in files_to_download:
+        if file_name in file_list:
+            file_path = os.path.join(FILES_DIR, file_name)
+            if os.path.exists(file_path):
+                file_size = os.path.getsize(file_path)
+                send_file_start(conn, file_name, file_size)
+                
+                with open(file_path, 'rb') as f:
+                    while chunk := f.read(CHUNK_SIZE):
+                        send_file_chunk(conn, file_name, chunk)
+
+                send_file_end(conn, file_name)
+            else:
+                send_error(conn, f'{file_name} not found in {FILES_DIR}')
+        else:
+            send_error(conn, f'{file_name} not found in file list')
+
+def send_error(conn, message):
+    """Send an error message to the client."""
+    conn.sendall(json.dumps({'type': 'error', 'message': message}).encode())
+
+def handle_client(conn, addr, file_list):
+    """Handle the communication with a connected client."""
+    global client_conn
+    client_conn = conn  # Store the client connection globally
+    print(f'Connected by {addr}')
+    send_file_list(conn, file_list)
+    
     while not shutdown_flag:
         try:
             data = conn.recv(1024)
@@ -43,62 +95,49 @@ def handle_client(conn, addr, file_list):
             if request.get('action') == 'shutdown':
                 print(f'Client {addr} is shutting down.')
                 break
-            if request.get('action') == 'download':
-                files_to_download = request['files']
-
-                for file_name in files_to_download:
-                    if file_name in file_list:
-                        file_path = os.path.join(FILES_DIR, file_name)
-                        if os.path.exists(file_path):
-                            file_size = os.path.getsize(file_path)
-
-                            # Send start of file marker
-                            conn.sendall(json.dumps({'type': 'start', 'filename': file_name, 'size': file_size}).encode())
-
-                            with open(file_path, 'rb') as f:
-                                while chunk := f.read(CHUNK_SIZE):
-                                    checksum = generate_checksum(chunk)
-                                    conn.sendall(json.dumps({'type': 'chunk', 'filename': file_name, 'chunk': chunk.decode('latin1'), 'checksum': checksum}).encode())
-
-                            # Send end of file marker
-                            conn.sendall(json.dumps({'type': 'end', 'filename': file_name}).encode())
-                        else:
-                            conn.sendall(json.dumps({'type': 'error', 'message': f'{file_name} not found in {FILES_DIR}'}).encode())
-                    else:
-                        conn.sendall(json.dumps({'type': 'error', 'message': f'{file_name} not found in file list'}).encode())
+            elif request.get('action') == 'download':
+                handle_download_request(conn, file_list, request['files'])
         except Exception as e:
             if not shutdown_flag:
                 print(f'Error: {e}')
             break
-                    
+
     conn.close()
 
-# Signal handler to set shutdown flag on Ctrl+C
+def signal_client_shutdown(conn):
+    """Signal the connected client to shut down."""
+    try:
+        if conn:
+            conn.sendall(json.dumps({'action': 'shutdown'}).encode())
+    except Exception as e:
+        print(f"Error signaling client shutdown: {e}")
+
 def signal_handler(sig, frame):
+    """Signal handler to set the shutdown flag on Ctrl+C."""
     global shutdown_flag
     print("Shutting down the server...")
+    signal_client_shutdown(client_conn)  # Signal the client before shutting down
     shutdown_flag = True
 
-# Main server function
-def main():
+def start_server():
+    """Start the file server."""
     global shutdown_flag
-    file_list = load_file_list()
+    file_list = load_file_list(FILES_LIST)
 
-    # Register the signal handler for SIGINT (Ctrl+C)
     signal.signal(signal.SIGINT, signal_handler)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)  # Allow only 1 client at a time
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)  # Allow only 1 client at a time
         print(f'Server listening on {HOST}:{PORT}')
 
         while not shutdown_flag:
             try:
-                s.settimeout(1.0)
-                conn, addr = s.accept()
+                server_socket.settimeout(1.0)
+                conn, addr = server_socket.accept()
                 handle_client(conn, addr, file_list)
             except socket.timeout:
                 continue
 
 if __name__ == "__main__":
-    main()
+    start_server()
